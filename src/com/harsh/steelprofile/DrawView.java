d721b7cb3aa87167f6263b42db4b760d03a7f2b3
Harsh Bhanvadia <harsh.slade@gmail.com>
2013-01-22 20:13:54 +0100

    Add ability to draw centroid and drawing axis
    Signed-off-by: Harsh Bhanvadia <harsh.slade@gmail.com>

--- src/com/harsh/steelprofile/DrawView.java
+++ src/com/harsh/steelprofile/DrawView.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2013 Harsh Bhanvadia <bhanvadia@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.harsh.steelprofile;
 
 import java.util.ArrayList;
@@ -21,6 +37,7 @@ public class DrawView extends View {
 	private static int INVALID_POINTER_ID = -1;    
     
 	Paint mPaint = new Paint();
+	Paint cPaint = new Paint();
 	private ArrayList<Command> mCommands;
 	int mWindowWidth;
 	int mWindowHeight;
@@ -192,6 +209,7 @@ public class DrawView extends View {
 		mScreenBitmap.recycle();
 	}
 	
+	// Panning and Zooming
 	@Override
 	public boolean onTouchEvent(MotionEvent event) {
 		final int pointerIndex;
@@ -287,7 +305,12 @@ public class DrawView extends View {
 	    	}
 	    }
 	    
-	    canvas.drawBitmap(mScreenBitmap, 0, 0, mPaint);
+	    // Draw center of mass
+	    cPaint.setColor(Color.RED);
+	    canvas.drawCircle(mCx/mA, mCy/mA, 5, cPaint);
+	    canvas.drawLine(mMidX - 20, mMidY + 10, mMidX + 30, mMidY + 10, cPaint);
+	    canvas.drawLine(mMidX - 10, mMidY + 20, mMidX - 10, mMidY - 30, cPaint);
+	    canvas.drawBitmap(mScreenBitmap, 0, 0, cPaint);
 	    
 	    if(mShowIcon && mAnimate) {
 	    	canvas.drawBitmap(mIcon, mCurrX - (mIcon.getWidth() / 2), mCurrY - mIcon.getHeight(), mPaint);
